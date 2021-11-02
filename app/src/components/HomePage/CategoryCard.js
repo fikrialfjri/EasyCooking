75e04ee9303fa59134338d3e5146ad156cf75491
fikrialfjri <fikribusiness2021@gmail.com>
2021-11-02 19:47:03 +0700

    add infinite scroll when fetch ingredients

--- app/src/components/HomePage/CategoryCard.js
+++ app/src/components/HomePage/CategoryCard.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 
 export default function CategoryCard({ category }) {
   return (
-    <div className="item bg-dark text-white rounded-2">
+    <div className="item text-white rounded-2" style={{ backgroundColor: "#E9EDF1" }}>
       <Link to={`/recipes/filter-by-category=${category.strCategory}`} className="text-dark text-decoration-none">
-        <Card.Img src={category.strCategoryThumb} alt={`${category.strCategory} Image`} fluid />
+        <Card.Img className="p-5" src={category.strCategoryThumb} alt={`${category.strCategory} Image`} fluid />
         <Card.ImgOverlay>
           <Card.Title className="p-2 rounded-2 text-center position-absolute" style={{ backgroundColor: "#FB7600" }}>{category.strCategory}</Card.Title>
         </Card.ImgOverlay>
