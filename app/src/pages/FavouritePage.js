9d9a259e2c3cfdae217ff520a949b39188b7458b
fikrialfjri <fikribusiness2021@gmail.com>
2021-11-11 08:00:34 +0700

    add coming soon image on favourite page

--- app/src/pages/FavouritePage.js
+++ app/src/pages/FavouritePage.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import comingSoon from "../assets/img/coming-soon.png"
+import { Image } from 'react-bootstrap'
 
 export default function FavoritPage() {
   return (
-    <div>
-      FavoritPage
+    <div style={{ backgroundColor: "#E9EDF1" }}>
+      <Image
+        src={comingSoon}
+        alt="coming-soon-image"
+        className="mx-auto d-block"
+        fluid
+      />
     </div>
   )
 }
